69a8bf66986bc7977703edcf5c62ed237ccf3538
Zeenan <jsnjover@gmail.com>
2016-03-10 17:32:57 +0800

    Extended Auth to Base

--- src/main/java/com/papafranku/rest/api/users/AuthenticationController.java
+++ src/main/java/com/papafranku/rest/api/users/AuthenticationController.java
@@ -14,7 +14,7 @@ import com.papafranku.entities.ResponseStatus;
 
 @RestController
 @RequestMapping("/api/users")
-public class AuthenticationController {
+public class AuthenticationController extends BaseController{
 	
 	public AuthenticationController() {
 		
